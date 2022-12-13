b990f639f53df6f615e4dbf19e049ebe1627affb
Bridget Akellian <akellianbv@gmail.com>
2022-12-13 10:14:36 -0500

    updated with notes

--- phase-3/09-sinatra-crud/pizza_maker_api/app/controllers/toppings_controller.rb
+++ phase-3/09-sinatra-crud/pizza_maker_api/app/controllers/toppings_controller.rb
@@ -10,5 +10,24 @@ class ToppingsController < ApplicationController
         topping.to_json
     end
 
+    post 'toppings' do 
+        Topping.create(
+            name: params[:name]
+        ).to_json
+    end
+
+    patch '/toppings/:id' do 
+        topping = Topping.find(params[:id])
+        topping.update(
+            name: params[:name]
+        ) #need to put topping before .to_json because .update does not return an instance of topping. update rtrns a boolean
+        topping.to_json
+    end 
+
+    delete '/toppings/:id' do 
+        topping = Topping.find(params[:id])
+        topping.destroy
+    end
+
 
 end
